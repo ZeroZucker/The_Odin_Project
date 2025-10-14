0f431f0af8cc533b58482ec02355a524523f60fd
ZeroZucker <zero1402zucker@gmail.com>
2025-10-14 15:46:01 +0200

    implemented the game loop

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const actions = ["rock", "paper", "scissors"];
+const conclusions = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
 
+let humanScore = 0, computerScore = 0;
 function getComputerChoice() {
     // should randomly return rock, paper or scissors
   //
@@ -15,3 +17,26 @@ function getHumanChoice() {
   return humanChoice.toLowerCase()
 }
 
+function playRound(humanChoice, computerChoice) {
+  // check if conclusion[humanchoie
+  if (computerChoice === conclusions[humanChoice]) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    humanScore += 1;
+  }
+   if (humanChoice === conclusions[computerChoice]) {
+    console.log(`You lose! ${humanChoice} loses to ${computerChoice}`) 
+    computerScore += 1
+  }
+
+  else {
+    console.log("tie" + humanChoice + computerChoice);
+  }
+}
+
+function playGame(){
+  for (let counter = 0; counter < 5; counter++) {
+    playRound(getHumanChoice(), getComputerChoice())
+    console.log(`Your score: ${humanScore} \n Computer score: ${computerScore}`)
+  }}
+
+playGame()
