cf8e6eec8ff03b36e9de0d0b03b28246a38ef0d1
ZeroZucker <zero1402zucker@gmail.com>
2025-10-16 17:54:09 +0200

    added the Book constructor

---
+++ Odin_Project/java-script-course/book-constructor.js
@@ -0,0 +1,12 @@
+function Book(title, author, pages, read) {
+
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+}
+
+let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 456, true);
+
+// console.log(theHobbit.info());
